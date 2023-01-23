5a0fdb4179b27311b6c9c611e54ed5b287d03d58
yashupadhyaya <uyash73@gmail.com>
2023-01-23 15:04:39 +0530

    change 3

--- aws-ts-ecs-anywhere/index.ts
+++ aws-ts-ecs-anywhere/index.ts
@@ -29,58 +29,58 @@ const numberNodes = projectConfig.getNumber("numberNodes") || 2;
 //   role: ssmRole,
 // });
 
-// const executionRole = new aws.iam.Role("taskExecutionRole", {
-//   assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
-//     aws.iam.Principals.EcsTasksPrincipal,
-//   ),
-// });
+const executionRole = new aws.iam.Role("taskExecutionRole", {
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
+    aws.iam.Principals.EcsTasksPrincipal,
+  ),
+});
 
-// const ecsTaskExecutionRoleAttachment = new aws.iam.RolePolicyAttachment("rpa-ecsanywhere-ecstaskexecution", {
-//   role: executionRole,
-//   policyArn: aws.iam.ManagedPolicy.AmazonECSTaskExecutionRolePolicy,
-// });
+const ecsTaskExecutionRoleAttachment = new aws.iam.RolePolicyAttachment("rpa-ecsanywhere-ecstaskexecution", {
+  role: executionRole,
+  policyArn: aws.iam.ManagedPolicy.AmazonECSTaskExecutionRolePolicy,
+});
 
-// const taskRole = new aws.iam.Role("taskRole", {
-//   assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
-//     aws.iam.Principals.EcsTasksPrincipal,
-//   ),
-// });
+const taskRole = new aws.iam.Role("taskRole", {
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
+    aws.iam.Principals.EcsTasksPrincipal,
+  ),
+});
 
-// const taskRolePolicy = new aws.iam.RolePolicy("taskRolePolicy", {
-//   role: taskRole.id,
-//   policy: {
-//     Version: "2012-10-17",
-//     Statement: [
-//       {
-//         Effect: "Allow",
-//         Action: [
-//           "ssmmessages:CreateControlChannel",
-//           "ssmmessages:CreateDataChannel",
-//           "ssmmessages:OpenControlChannel",
-//           "ssmmessages:OpenDataChannel",
-//         ],
-//         Resource: "*",
-//       },
-//       {
-//         Effect: "Allow",
-//         Action: ["logs:DescribeLogGroups"],
-//         Resource: "*",
-//       },
-//       {
-//         Effect: "Allow",
-//         Action: [
-//           "logs:CreateLogStream",
-//           "logs:CreateLogGroup",
-//           "logs:DescribeLogStreams",
-//           "logs:PutLogEvents",
-//         ],
-//         Resource: "*",
-//       },
-//     ],
-//   },
-// });
+const taskRolePolicy = new aws.iam.RolePolicy("taskRolePolicy", {
+  role: taskRole.id,
+  policy: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "ssmmessages:CreateControlChannel",
+          "ssmmessages:CreateDataChannel",
+          "ssmmessages:OpenControlChannel",
+          "ssmmessages:OpenDataChannel",
+        ],
+        Resource: "*",
+      },
+      {
+        Effect: "Allow",
+        Action: ["logs:DescribeLogGroups"],
+        Resource: "*",
+      },
+      {
+        Effect: "Allow",
+        Action: [
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents",
+        ],
+        Resource: "*",
+      },
+    ],
+  },
+});
 
-//Set up SSM
+// Set up SSM
 // const ssmActivation = new aws.ssm.Activation("ecsanywhere-ssmactivation", {
 //   iamRole: ssmRole.name,
 //   registrationLimit: numberNodes,
